9b315e1cd31e4ce769f96fdee8c44137139ea594
auroremayo <auroremayo@hotmail.fr>
2026-03-19 21:58:37 +0100

    pull models from DagsHub

--- backend/app/services/ml_service.py
+++ backend/app/services/ml_service.py
@@ -3,10 +3,12 @@ Chargement et utilisation du modèle LightGBM + TF-IDF.
 """
 
 import logging
+import subprocess
 import joblib
 
 from ..core.config import MODEL_PATH, VECTORIZER_PATH
 from nlp_pipeline import processing_pipeline
+from common.setup_dvc import setup_dvc
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +23,12 @@ def get_model():
     global _model, _vectorizer
     if _model is None:
         try:
+            setup_dvc()
+  
+            # pull models depuis DVC
+            subprocess.run(["dvc", "pull", MODEL_PATH], check=True)
+            subprocess.run(["dvc", "pull", VECTORIZER_PATH], check=True)
+
             _model      = joblib.load(MODEL_PATH)
             _vectorizer = joblib.load(VECTORIZER_PATH)
             logger.info("Modèle LightGBM et TF-IDF chargés avec succès.")
